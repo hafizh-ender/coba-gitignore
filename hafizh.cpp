eec4e4c9426a331a73af22b63e0421d0afcd4604
Hafizh Renanto Akhmad <hafizhrenanto88@gmail.com>
2022-02-11 17:25:49 +0700

    Add C++ version

---
+++ hafizh.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main() {
+    printf("The name's Hafizh.\n");
+    printf("I like to play games.\n");
+    printf("I like to watch anime.\n");
+    printf("I'd like to go to Europe and Japan.\n");
+}
